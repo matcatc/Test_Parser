62848b71282ed77dde3a90a64fd89fff8875c420
Matthew A. Todd <matcatprg@yahoo.com>
2010-02-17 18:51:31 -0800

    added doctest info

--- src/TestResults/TestCase.py
+++ src/TestResults/TestCase.py
@@ -8,6 +8,14 @@ class TestCase:
     '''
     issue: if messages and errors are stored in separate data structures,
     the individual order will be lost
+    
+    >>> testCase = TestCase()
+    >>> testCase.hasError()
+    False
+        
+    >>> testCase.addError(0)    # not real error type, but works for test
+    >>> testCase.hasError()
+    True
     '''
 
     errors = ([])
@@ -25,12 +33,16 @@ class TestCase:
         else:
             return False
         
+    def setTimeTaken(self, time):
+        self.timeTaken = time
+        
     def addError(self, error):
-        self.errors.add(error)
+        self.errors.append(error)
         
     def addMessage(self, message):
         '''
-        appends the error message to the list, so that the messages are kept in order
+        appends the error message to the list,
+         so that the messages are kept in order
         '''
         self.messages.append(message)
         
