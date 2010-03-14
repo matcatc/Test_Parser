52f1403f058e745c7b2fb6c0238472886b038a33
Matthew A. Todd <matcatprg@yahoo.com>
2010-03-13 17:06:08 -0800

    Implemented About.py
    Using proper pyQt method

--- src/BoostTestParser/View/About.py
+++ src/BoostTestParser/View/About.py
@@ -1,47 +1,31 @@
-# -*- coding: utf-8 -*-
+'''
+Created on Mar 13, 2010
 
-# Form implementation generated from reading ui file 'About.ui'
-#
-# Created: Fri Mar 12 11:46:30 2010
-#      by: PyQt4 UI code generator 4.6
-#
-# WARNING! All changes made in this file will be lost!
+@author: matcat
+'''
 
-from PyQt4 import QtCore, QtGui
+from PyQt4 import uic #@UnresolvedImport
+from PyQt4 import QtGui #@UnresolvedImport
+import sys
 
-class Ui_AboutDialog(object):
-    def setupUi(self, AboutDialog):
-        AboutDialog.setObjectName("AboutDialog")
-        AboutDialog.resize(378, 262)
-        self.verticalLayout = QtGui.QVBoxLayout(AboutDialog)
-        self.verticalLayout.setObjectName("verticalLayout")
-        self.horizontalLayout = QtGui.QHBoxLayout()
-        self.horizontalLayout.setObjectName("horizontalLayout")
-        spacerItem = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
-        self.horizontalLayout.addItem(spacerItem)
-        self.label = QtGui.QLabel(AboutDialog)
-        self.label.setObjectName("label")
-        self.horizontalLayout.addWidget(self.label)
-        spacerItem1 = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
-        self.horizontalLayout.addItem(spacerItem1)
-        self.verticalLayout.addLayout(self.horizontalLayout)
-        self.label_2 = QtGui.QLabel(AboutDialog)
-        self.label_2.setObjectName("label_2")
-        self.verticalLayout.addWidget(self.label_2)
-        spacerItem2 = QtGui.QSpacerItem(20, 40, QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Expanding)
-        self.verticalLayout.addItem(spacerItem2)
-        self.buttonBox = QtGui.QDialogButtonBox(AboutDialog)
-        self.buttonBox.setOrientation(QtCore.Qt.Horizontal)
-        self.buttonBox.setStandardButtons(QtGui.QDialogButtonBox.Close)
-        self.buttonBox.setObjectName("buttonBox")
-        self.verticalLayout.addWidget(self.buttonBox)
+UiClass, WidgetClass = uic.loadUiType("./About.ui")
 
-        self.retranslateUi(AboutDialog)
-        QtCore.QObject.connect(self.buttonBox, QtCore.SIGNAL("accepted()"), AboutDialog.accept)
-        QtCore.QMetaObject.connectSlotsByName(AboutDialog)
 
-    def retranslateUi(self, AboutDialog):
-        AboutDialog.setWindowTitle(QtGui.QApplication.translate("AboutDialog", "About Test Parser", None, QtGui.QApplication.UnicodeUTF8))
-        self.label.setText(QtGui.QApplication.translate("AboutDialog", "TestParser", None, QtGui.QApplication.UnicodeUTF8))
-        self.label_2.setText(QtGui.QApplication.translate("AboutDialog", "I built Test Parser to ...", None, QtGui.QApplication.UnicodeUTF8))
+class About(UiClass, WidgetClass):
+    '''
+    classdocs
+    '''
 
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        WidgetClass.__init__(self)
+        self.setupUi(self)
+        
+        
+if __name__ == "__main__":
+    app = QtGui.QApplication(sys.argv)
+    widget = About()
+    widget.show()
+    sys.exit(app.exec_())
