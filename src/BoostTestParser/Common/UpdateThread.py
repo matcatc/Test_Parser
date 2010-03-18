d70b18af546ce9f35569d0eb82367848130518b5
Matthew A. Todd <matcatprg@yahoo.com>
2010-03-18 12:35:05 -0700

    moved lock into run()
    added comments

--- src/BoostTestParser/Common/UpdateThread.py
+++ src/BoostTestParser/Common/UpdateThread.py
@@ -4,22 +4,22 @@ Created on Mar 14, 2010
 @author: matcat
 '''
 import threading, queue
-#import sys     # for debug statement
 
-class UpdateThread ( threading.Thread ):
+class UpdateThread (threading.Thread):
     '''
-    Job is to update a given target. So jobPool contains observers
+    Job is to update a given target. So jobPool contains observers.
     '''
     # Note that these are static variables
+    
+    # TODO restrict access to jobPool
+    #  property: read only
+    #  or make private and provide helper function for join()
     jobPool = queue.Queue(0)
 
-    bPoolCreated = False
-
-    _threadCount = 0    # might want to use a property to keep clients from changing this value.
-
-
-    _removeCount = 0    # might want to use a property to keep clients from changing this value
-    lock = threading.Lock() # for use when removing threads (in run())
+    _bPoolCreated = False
+    
+    _threadCount = 0
+    _removeCount = 0
 
     @staticmethod
     def createPool(numThreads):
@@ -27,9 +27,9 @@ class UpdateThread ( threading.Thread ):
         Creates the initial pool of threads.
         If called more than once, it doesn't do anything subsequent times.
         '''
-        if not UpdateThread.bPoolCreated:
+        if not UpdateThread._bPoolCreated:
             print("creating thread pool")
-            UpdateThread.bPoolCreated = True
+            UpdateThread._bPoolCreated = True
             UpdateThread.addThreads(numThreads)
 
     @staticmethod
@@ -56,28 +56,31 @@ class UpdateThread ( threading.Thread ):
         
     @staticmethod
     def addJob(observer):
-        UpdateThread.jobPool.put(observer)
-        
-
-    def run ( self ):
         '''
-        will only return if _removeCount > 0
+        add a job to the jobPool
+        '''
+        UpdateThread.jobPool.put(observer)
+
+    def run (self):
+        '''
+        will only return (die off) if _removeCount > 0
         '''
         while True:
             observer = UpdateThread.jobPool.get()
 
             # job processing
             if observer != None:
-                #print("DEBUG: updating target:" , observer, file=sys.stderr)
                 observer.update()
             else:
                 print("cannot process non-existent observer")
                 
             # notify queue that job is done
             UpdateThread.jobPool.task_done()
-
+            
             # whether thread should die off (in order to remove threads)
-            with UpdateThread.lock:
+            lock = threading.Lock()
+            with lock:
+                #TODO: helper function?
                 if UpdateThread._removeCount > 0:
                     UpdateThread._removeCount -= 1
                     print("Removing thread")
