7cf669878284379d54331d5f1c74764eb0963e57
Matthew A. Todd <matcatprg@yahoo.com>
2010-05-13 20:13:47 -0700

    implemented tests

--- src/tests/View/TextView_Test.py
+++ src/tests/View/TextView_Test.py
@@ -4,22 +4,62 @@
 '''
 import unittest
 
-from BoostTestParser.View.TextView import TextView
+from BoostTestParser.View import TextView
+from BoostTestParser.Model import Model, TestRunner
+from BoostTestParser.Parser import BasicParser
+
 
 
 class TextView_Test(unittest.TestCase):
-
+    '''
+    I am not really sure what and how to test for this class.
+    
+    The only thing to really test with the display() methods is
+    that there are no misspellings. But that would require feeding
+    in data that would cause the methods to be completely executed.
+    '''
 
     def setUp(self):
-        pass
+        self.model = Model.Model()
+        self.view = TextView.TextView(self.model)
 
 
     def tearDown(self):
-        pass
+        del self.model
+        del self.view
 
 
-    def testName(self):
-        raise NotImplementedError
+    def test_registering(self):
+        '''
+        Test that the view registers itself with the model on creation
+        '''
+        self.assertTrue(self.view in self.model.observers)
+
+    def test_update(self):
+        '''
+        checks that there are no exceptions raised. Won't actually do
+        much of anything.
+        '''
+        self.view.update()
+
+    def test_startView(self):
+        '''
+        checks that no exceptions raised.
+        
+        Run twice just to see any problems occur (not actually looking
+        for though.)
+        
+        Will fail if "tests/Model/Boost_Test" doesn't point to a real
+        test runner.
+        '''
+        model = Model.Model()
+        runner = TestRunner.TestRunner()
+        runner.runner = "tests/Model/Boost_Test"
+        model.testRunner = runner
+        model.parser = BasicParser.BasicParser()
+
+        TextView.TextView.startView(model)
+        TextView.TextView.startView(model)
 
 
 if __name__ == "__main__":
