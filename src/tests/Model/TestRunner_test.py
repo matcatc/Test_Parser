d38f7d342db416cfece49dfdae55bbb2a97dde4a
Matthew A. Todd <matcatprg@yahoo.com>
2010-05-21 11:26:12 -0700

    added licensing information
    closes gh-24

--- src/tests/Model/TestRunner_test.py
+++ src/tests/Model/TestRunner_test.py
@@ -1,7 +1,24 @@
 '''
 @date Mar 6, 2010
 @author: Matthew A. Todd
+
+This file is part of Test Parser
+by Matthew A. Todd
+
+Test Parser is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Test Parser is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Test Parser.  If not, see <http://www.gnu.org/licenses/>.
 '''
+
 import unittest
 from BoostTestParser.Model.TestRunner import TestRunner
 from BoostTestParser.Common.Constants import Constants
--- src/tests/Parser/BasicParser_Test.py
+++ src/tests/Parser/BasicParser_Test.py
@@ -1,8 +1,24 @@
 '''
-Created on Feb 27, 2010
+@date Feb 27, 2010
+@author: Matthew A. Todd
 
-@author: matcat
+This file is part of Test Parser
+by Matthew A. Todd
+
+Test Parser is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Test Parser is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Test Parser.  If not, see <http://www.gnu.org/licenses/>.
 '''
+
 import unittest
 from BoostTestParser.Parser.BasicParser import BasicParser
 from xml.etree import ElementTree as ET
