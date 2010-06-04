dbb93db550e12f85b7d16ce6fd4099855bf147c8
Matthew A. Todd <matcatprg@yahoo.com>
2010-06-04 11:09:34 -0700

    added initial setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+'''
+@date Jun 4, 2010
+@author Matthew A. Todd
+'''
+
+from distutils.core import setup
+
+setup(name='Test Parser',
+      version='0.1',
+      packages=['src/BoostTestParser', 'src/tests']
+      )
+
