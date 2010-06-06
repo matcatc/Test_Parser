874bdf60ce222c5ad5b268d5604073747317ddae
Matthew A. Todd <matcatprg@yahoo.com>
2010-06-06 13:27:02 -0700

    still working on setup.py

--- setup.py
+++ setup.py
@@ -5,23 +5,33 @@
 '''
 
 from distutils.core import setup
+#from setuptools import setup, find_packages
 
 setup(name='Test Parser',
         version='0.1',
-        packages=['src/TestParser',
-                'src/TestParser.Common',
-                'src/TestParser.Model',
-                'src/TestParser.Parser',
-                'src/TestParser.TestResults',
-                'src/TestParser.View',
-                'src/tests',
-                'src/tests.Common',
-                'src/tests.Model',
-                'src/tests.Parser',
-                'src/tests.TestResults',
-                'src/tests.View',
+        description='Test Parser displays the results from a given test run',
+        author='Matthew A. Todd',
+        author_email='matcatprg@yahoo.com',
+        url='http://github.com/matcatc/Test_Parser',
+        
+        package_dir = {"": "src"},
+        packages=['TestParser',
+                'TestParser.Common',
+                'TestParser.Model',
+                'TestParser.Parser',
+                'TestParser.TestResults',
+                'TestParser.View',
+                'tests',
+                'tests.Common',
+                'tests.Model',
+                'tests.Parser',
+                'tests.TestResults',
+                'tests.View',
                 ],
-        py_modules=['src/main', 'src/test_runner'],    
-        scripts=['src/main.py', 'src/test_runner.py']
+#        packages = find_packages(),
+        scripts=['src/main.py', 'src/test_runner.py'],
+        
+        package_data = {'TestParser.View' : ['*.ui']}
+        # TODO: data files (docs)
         )
 
