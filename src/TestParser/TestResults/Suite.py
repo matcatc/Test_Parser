0eb953a0ba2126b173e12e7a56e4af8322287ada
Matthew A. Todd <matcatprg@yahoo.com>
2010-06-13 12:55:12 -0700

    added constructor arg name
    allow for suite to be named through provided methods

--- src/TestParser/TestResults/Suite.py
+++ src/TestParser/TestResults/Suite.py
@@ -22,21 +22,24 @@ class Suite:
     '''
     Contains an entire suite of tests. Organized into test cases.
     
+    Basically just a glorified set.
+    
 
     @date Feb 17, 2010
     @author Matthew A. Todd
     '''
 
-    def __init__(self):
+    def __init__(self, name = ""):
         '''
         Constructor
         '''
         self.testCases = set([])
-        self.name = ""
+        self.name = name
         
     def testCount(self):
         '''
         Number of tests cases contained in this suite
+        
         @return number of tests contained in suite.
         '''
         return len(self.testCases)
