cbd389a417ac83d88b78549b17bbcaa44d7771dc
Matthew A. Todd <matcatprg@yahoo.com>
2010-06-28 12:57:38 -0700

    added example runner for python unittest

---
+++ src/TestParser_tests/Model/python_unittest_example.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+'''
+This is a python unittest suite to be used as sample input for Test Parser
+(running and TestParser's unittests.)
+
+It also serves as an example for end users so that they can see what will
+work.
+
+Note: this example is from Python's Unittest documentation, with minor
+changes to make it work with both python 2.x and 3.x.
+
+@date Jun 28, 2010
+@author Matthew A. Todd
+'''
+import random
+import unittest
+import sys
+
+class TestSequenceFunctions(unittest.TestCase):
+
+	def setUp(self):
+		self.seq = [i for i in range(10)]
+
+	def test_shuffle(self):
+		# make sure the shuffled sequence does not lose any elements
+		random.shuffle(self.seq)
+		self.seq.sort()
+		self.assertEqual(self.seq, [i for i in range(10)])
+
+	def test_choice(self):
+		element = random.choice(self.seq)
+		self.assertTrue(element in self.seq)
+
+	def test_sample(self):
+		self.assertRaises(ValueError, random.sample, self.seq, 20)
+		for element in random.sample(self.seq, 5):
+			self.assertTrue(element in self.seq)
+
+	def test_fail(self):
+		self.fail()
+
+if __name__ == '__main__':
+	suite = unittest.TestLoader().loadTestsFromTestCase(TestSequenceFunctions)
+	unittest.TextTestRunner(verbosity=2, stream=sys.stdout).run(suite)
+
+
