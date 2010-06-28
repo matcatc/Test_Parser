234205be12284d176028160dc194e825888525d4
Matthew A. Todd <matcatprg@yahoo.com>
2010-06-28 12:40:48 -0700

    added to docstring

--- src/TestParser/Model/PythonUnittestRunner.py
+++ src/TestParser/Model/PythonUnittestRunner.py
@@ -8,6 +8,14 @@ class PythonUnittestRunner(IRunner):
     '''
     Runner for Python Unittest framework.
     
+    @warning Because of how Python Unittest is designed, we require
+    that the test runner be setup a certain way. For parsing to work,
+    we need the verbosity level to be set to 2. For running to work,
+    we need the information all output to stdout. I.e: use the
+    following command in your test runner.
+    
+    unittest.TextTestRunner(verbosity=2, stream=sys.stdout).run(suite)
+    
     @date Jun 28, 2010
     @author matcat
     '''
