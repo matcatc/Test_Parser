0e2ae9473722c72203e04adf1db8dc43eec57878
Matthew A. Todd <matcatprg@yahoo.com>
2010-06-30 14:48:36 -0700

    implemented tests for python unittest parser
    added sample file

--- src/TestParser_tests/Parser/PythonUnittestParser_Test.py
+++ src/TestParser_tests/Parser/PythonUnittestParser_Test.py
@@ -6,20 +6,46 @@ Created on Jun 30, 2010
 import unittest
 
 from TestParser.Parser.PythonUnittestParser import PythonUnittestParser
+from TestParser.Common.computeDataFilepath import computeDataFilepath
 
 class PythonUnittestParser_Test(unittest.TestCase):
 
 
     def setUp(self):
-        pass
+        self.parser = PythonUnittestParser()
 
 
     def tearDown(self):
-        pass
+        del self.parser
 
-
-    def testName(self):
-        pass
+    def test_validStatusLine(self):
+        self.assertTrue(self.parser._validStatusLine("test_choice123 (__main__.TestSequenceFunctions123) ... ok"))
+        self.assertTrue(self.parser._validStatusLine("test_error (__main__.TestSequenceFunctions) ... ERROR"))
+        self.assertTrue(self.parser._validStatusLine("test_fail (__main__.TestSequenceFunctions) ... FAIL"))
+        
+    def test_validFailLine(self):
+        self.assertTrue(self.parser._validFailLine("FAIL: test_fail123 (__main__.TestSequenceFunctions123)"))
+        self.assertTrue(self.parser._validFailLine("ERROR: test_error (__main__.TestSequenceFunctions)"))
+        
+    def test_validFailInfoLine(self):
+        self.assertTrue(self.parser._validFailInfoLine('  File "python_unittest_example.py", line 43, in test_error'))
+        self.assertTrue(self.parser._validFailInfoLine('  File "python_unittest_example.123asd", line 40123, in test_fail'))
+    
+    def test_ParseFile(self):
+        f = open(computeDataFilepath("python_unittest_sample", __file__))
+        self.parser.parse(file = f)
+        
+    def test_ParseStringdata(self):
+        f = open(computeDataFilepath("python_unittest_sample", __file__))
+        self.parser.parse(stringData = f.read())
+        
+    def test_ParseNone(self):
+        '''
+        Test that nothing explodes when we don't give it data to parse.
+        
+        Note: later we might change to checking for an exception.
+        '''
+        self.parser.parse(None)
 
 
 if __name__ == "__main__":
