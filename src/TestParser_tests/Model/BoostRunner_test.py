59f32f8c2b68f802b1469cb8df214b7b3f2cf5e1
Matthew A. Todd <matcatprg@yahoo.com>
2010-06-30 14:24:09 -0700

    moved appropriate tests
    from BoostRunner test suite to IRunner test sutie

--- src/TestParser_tests/Model/BoostRunner_test.py
+++ src/TestParser_tests/Model/BoostRunner_test.py
@@ -55,69 +55,10 @@ class BoostRunner_Test(unittest.TestCase):
     def tearDown(self):
         Constants.reset()
         del self.runner
-        
-    def test_runnerDeleter(self):
-        '''
-        b/c the deleter isn't being run, we're going to just run it here.
-        '''
-        self.runner.runner = "blah"
-        del self.runner.runner
 
-    def testRun_echo(self):
-        '''
-        test run() using echo.
-        the "-n" option tells echo not to return a newline
-        '''
-        input = "echo test output"
-        output = BoostRunner.LOG_FORMAT + " --log_level=test_suite " + input + "\n"
-        self.runner.runner = "echo"
-        stdout = self.runner.run([input])
-        self.assertEqual(stdout.decode("utf-8"), output)
-        
-    def testRunPrevious_echo(self):
-        '''
-        test runPrevious() using echo.
-        
-        if runPrevious() actually executes same cmd, then the output should be
-        the same.
-        '''
-        input = "echo test output"
-        self.runner.runner = "echo"
-        output1 = self.runner.run([input])
-        
-        output2 = self.runner.runPrevious()
-        
-        self.assertEqual(output1.decode("utf-8"), output2.decode("utf-8"))
-        
-    def testRunPrevious_Invalid(self):
-        '''
-        Test what happens when rerunning w/o a previous run. Should output
-        to log and execute runAll().
-        '''
-        input = "echo test output"
-        self.runner.runner = "echo"
-        
-        output1 = self.runner.runPrevious()
-        
-        output2 = self.runner.runAll()
-        
-        self.assertEqual(output1.decode("utf-8"), output2.decode("utf-8"))
-        
-        
-    def testRunAll_echo(self):
-        '''
-        test runAll() using echo.
-        output = "\n" b/c echo return a new line after printing (nothing in this case) 
-        '''
-        output = BoostRunner.LOG_FORMAT + " --log_level=test_suite\n"
-        self.runner.runner = "echo"
-        stdout = self.runner.runAll()
-        self.assertEqual(stdout.decode("utf-8"), output)
-        
     def testRunAll(self):
         '''
         test runAll() with real input
-        this test depends on the the filesystem location from which we are running this test
         '''
         self.runner.runner = computeDataFilepath("Boost_Test", __file__)
         stdout = self.runner.runAll()
@@ -156,25 +97,12 @@ class BoostRunner_Test(unittest.TestCase):
         stdout = self.runner.runSuite([])
         self.assertNotEqual(stdout, None)
         
-    def test_InvalidRunner(self):
+    def test_computeCmd(self):
         '''
-        test that when trying to run an invalid runner, nothing
-        crazy happens.
         '''
-        self.runner.runner = "invalid_runner_ALSKFJEOIJFDFLKJakjdflakjdfaedf"
-        self.runner.run([])
-
-
-    def test_NoneRunner(self):
-        '''
-        Test that return value is None when runner is None.
+        raise NotImplementedError
         
-        Do we want run() to raise an exception instead?
-        '''
-        self.runner.runner = None
-        stdout = self.runner.run([])
-        
-        self.assertEqual(stdout, None)
+
         
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
--- src/TestParser_tests/Model/IRunner_Test.py
+++ src/TestParser_tests/Model/IRunner_Test.py
@@ -5,20 +5,112 @@ Created on Jun 30, 2010
 '''
 import unittest
 
+from TestParser.Model.IRunner import IRunner
+
+class MockRunner(IRunner):
+    '''
+    IRunner leaves it to its children to implement computeCmd,
+    so we're subclassing and implementing the most basic version
+    so that we can test other functionality.
+    '''
+    def computeCmd(self, params):
+        return [self.runner] + params
+        
 
 class IRunner_Test(unittest.TestCase):
-
-
     def setUp(self):
-        pass
-
+        self.runner = MockRunner()
 
     def tearDown(self):
-        pass
+        del self.runner
+
+    
+    def test_runnerDeleter(self):
+        '''
+        b/c the deleter isn't being run, we're going to just run it here.
+        '''
+        self.runner.runner = "blah"
+        del self.runner.runner
+    
+    def test_InvalidRunner(self):
+        '''
+        test that when trying to run an invalid runner, nothing
+        crazy happens.
+        '''
+        self.runner.runner = "invalid_runner_ALSKFJEOIJFDFLKJakjdflakjdfaedf"
+        self.runner.run([])
 
 
-    def testName(self):
-        pass
+    def test_NoneRunner(self):
+        '''
+        Test that return value is None when runner is None.
+        
+        Do we want run() to raise an exception instead?
+        '''
+        self.runner.runner = None
+        stdout = self.runner.run([])
+        
+        self.assertEqual(stdout, None)
+        
+    
+    def testRun_echo(self):
+        '''
+        test run() using echo.
+        the "-n" option tells echo not to return a newline
+        '''
+        input = "echo test output"
+        output = input + "\n"
+        self.runner.runner = "echo"
+        stdout = self.runner.run([input])
+        self.assertEqual(stdout.decode("utf-8"), output)
+        
+    def testRunPrevious_echo(self):
+        '''
+        test runPrevious() using echo.
+        
+        if runPrevious() actually executes same cmd, then the output should be
+        the same.
+        '''
+        input = "test output"
+        self.runner.runner = "echo"
+        output1 = self.runner.run([input])
+        
+        output2 = self.runner.runPrevious()
+        
+        self.assertEqual(output1.decode("utf-8"), output2.decode("utf-8"))
+        
+    def testRunPrevious_Invalid(self):
+        '''
+        Test what happens when rerunning w/o a previous run. Should output
+        to log and execute runAll().
+        '''
+        input = "echo test output"
+        self.runner.runner = "echo"
+        
+        output1 = self.runner.runPrevious()
+        
+        output2 = self.runner.runAll()
+        
+        self.assertEqual(output1.decode("utf-8"), output2.decode("utf-8"))
+        
+        
+    def testRunAll_echo(self):
+        '''
+        test runAll() using echo.
+        output = "\n" b/c echo return a new line after printing (nothing in this case) 
+        '''
+        output = "\n"
+        self.runner.runner = "echo"
+        stdout = self.runner.runAll()
+        self.assertEqual(stdout.decode("utf-8"), output)
+    
+    
+    def test_computeCmd(self):
+        '''
+        Test that computeCmd() not implemented.
+        '''
+        runner = IRunner()
+        self.assertRaises(NotImplementedError, runner.computeCmd, None)
 
 
 if __name__ == "__main__":
