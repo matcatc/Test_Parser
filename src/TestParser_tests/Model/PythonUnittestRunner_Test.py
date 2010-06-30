26488466b7a607f4263fab9d38534201947682cd
Matthew A. Todd <matcatprg@yahoo.com>
2010-06-30 14:31:32 -0700

    implemented tests for Python Unittest Runner

--- src/TestParser_tests/Model/PythonUnittestRunner_Test.py
+++ src/TestParser_tests/Model/PythonUnittestRunner_Test.py
@@ -7,19 +7,36 @@ import unittest
 
 from TestParser.Model.PythonUnittestRunner import PythonUnittestRunner
 
+from TestParser.Common.computeDataFilepath import computeDataFilepath
+
 class PythonUnittestRunner_Test(unittest.TestCase):
 
 
     def setUp(self):
-        pass
+        self.runner = PythonUnittestRunner()
 
 
     def tearDown(self):
-        pass
+        del self.runner
 
 
-    def testName(self):
-        pass
+    def test_computeCmd(self):
+        '''
+        Test that the command returned is correct
+        '''
+        self.runner.runner = "<runner>"
+        input = ["input"]
+        output = [self.runner.runner]
+        self.assertEqual(self.runner.computeCmd(input), output)
+        
+    def testRunAll(self):
+        '''
+        test runAll() with real input
+        '''
+        self.runner.runner = computeDataFilepath("python_unittest_example.py", __file__)
+        stdout = self.runner.runAll()
+        self.assertNotEqual(stdout, None)
+        
 
 
 if __name__ == "__main__":
