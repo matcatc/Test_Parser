eeed3a28453ce6da6b06e876d4032c8279900d91
Matthew A. Todd <matcatprg@yahoo.com>
2010-06-30 13:59:20 -0700

    removed old test stuff

--- src/TestParser_tests/Parser/IParse_Test.py
+++ src/TestParser_tests/Parser/IParse_Test.py
@@ -20,36 +20,22 @@ along with Test Parser.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
 import unittest
-import os.path
 from TestParser.Parser import IParse
-from TestParser.Common.computeDataFilepath import computeDataFilepath
-
-class Mock_Parser(IParse.IParse):
-    '''
-    subclass IParse so that we can make sure that the correct
-    data is being passed to _parseData()
-    '''
-    def _parseData(self, tree):
-        self.tree = tree
         
-
 class IParse_Test(unittest.TestCase):
 
 
     def setUp(self):
         self.parser = IParse.IParse()
-        self.mockParser = Mock_Parser()
 
     def tearDown(self):
         del self.parser
 
     def testparse(self):
         '''
-        IParse's parse() should be undefined.
+        IParse's parse() should be unimplemented.
         '''
         self.assertRaises(NotImplementedError, self.parser.parse, None)
-        
-
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
