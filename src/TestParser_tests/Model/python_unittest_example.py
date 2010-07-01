207448ee1c8bfc206fd78df88152ba0e93875a4b
Matthew A. Todd <matcatprg@yahoo.com>
2010-07-01 13:49:57 -0700

    added licensing information

--- src/TestParser_tests/Model/python_unittest_example.py
+++ src/TestParser_tests/Model/python_unittest_example.py
@@ -11,7 +11,24 @@ changes to make it work with both python 2.x and 3.x.
 
 @date Jun 28, 2010
 @author Matthew A. Todd
+
+This file is part of Test Parser
+by Matthew A. Todd
+
+Test Parser is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Test Parser is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Test Parser.  If not, see <http://www.gnu.org/licenses/>.
 '''
+
 import random
 import unittest
 import sys
