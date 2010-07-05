2923b0ae4068c324b48b252df14fbe9c09cba917
Matthew A. Todd <matcatprg@yahoo.com>
2010-07-04 18:00:59 -0700

    added TODO

--- src/TestParser/Model/JUnitRunner.py
+++ src/TestParser/Model/JUnitRunner.py
@@ -19,6 +19,10 @@ class JUnitRunner(IRunner):
         Constructor
         
         @param version version number of Junit (i.e: 3 or 4).
+        
+        TODO: if we're leaving it up to the user to use the (correct)
+        JUnit test runner, then we don't need to know version information
+        here.
         '''
         super().__init__()
         
