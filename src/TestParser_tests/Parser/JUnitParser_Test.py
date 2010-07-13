ebd65f0828a6da912ef71ce76bb2e27133b164d8
Matthew A. Todd <matcatprg@yahoo.com>
2010-07-13 15:39:42 -0700

    added detailed test for JUnit Parser
    closes gh-62

--- src/TestParser_tests/Parser/JUnitParser_Test.py
+++ src/TestParser_tests/Parser/JUnitParser_Test.py
@@ -32,6 +32,56 @@ class JUnitParser_Test(unittest.TestCase):
     def test_badStatusLine(self):
         data = '..Faa;dkadfjasdl\n'
         self.parser.parse( stringData=data)
+        
+    def test_parse(self):
+        '''
+        Test and validate parse with real data.
+        '''
+        data = open(computeDataFilepath("./sample/JUnit4_out", __file__)).read()
+        
+        results = self.parser.parse(stringData = data)
+        
+        testPass = results.getChildren()[0]
+        noticePass = testPass.getChildren()[0]
+        self.assertEquals(noticePass.type, "pass")
+        
+        suite = results.getChildren()[1]
+        suiteData = suite.getRelevantDisplayData()
+        self.assertTrue(('name', 'test') in suiteData)
+        
+        tests = suite.getChildren()
+        self.assertEquals(len(tests), 3)
+        
+        test0 = tests[0]
+        test0_data = test0.getRelevantDisplayData()
+        self.assertTrue(('name', 'testTwo') in test0_data)
+        notice0 = test0.getChildren()[0]
+        self.assertEquals(notice0.type, 'fail')
+        notice0_data = notice0.getRelevantDisplayData()
+        self.assertTrue(('file', 'test.java') in notice0_data)
+        self.assertTrue(('line', '21') in notice0_data)
+        self.assertTrue(('info', 'java.lang.AssertionError: expected : < 5 > but was : < 4 >') in notice0_data)
+        
+        test1 = tests[1]
+        test1_data = test1.getRelevantDisplayData()
+        self.assertTrue(('name', 'TestThree') in test1_data)
+        notice1 = test1.getChildren()[0]
+        self.assertEquals(notice1.type, 'fail')
+        notice1_data = notice1.getRelevantDisplayData()
+        self.assertTrue(('file', 'test.java') in notice1_data)
+        self.assertTrue(('line', '27') in notice1_data)
+        self.assertTrue(('info', 'java.lang.Exception'))
+        
+        test2 = tests[2]
+        test2_data = test2.getRelevantDisplayData()
+        self.assertTrue(('name', 'testFour') in test2_data)
+        notice2 = test2.getChildren()[0]
+        self.assertEquals(notice2.type, 'fail')
+        notice2_data = notice2.getRelevantDisplayData()
+        self.assertTrue(('file', 'test.java') in notice2_data)
+        self.assertTrue(('line', '33') in notice2_data)
+        self.assertTrue(('info', 'java.lang.AssertionError') in notice2_data)
+        
 
 
 if __name__ == "__main__":
